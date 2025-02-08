57bde26ad6a35560c2d37fe95f003d025561e13e
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-02-08 19:29:09 +0530

    Add CORS middleware to server.js

--- back-end/server.js
+++ back-end/server.js
@@ -16,6 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+const cors= require('cors');
 
 // Middleware
 app.use(express.json());
